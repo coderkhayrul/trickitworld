b2519907ff0e2cd16768d042f0fb6035a2191adc
Coderkhayrul <coderkhayrul>
2021-08-25 05:10:32 +0600

    Facebook Page Link

--- resources/views/frontend/fronted_master.blade.php
+++ resources/views/frontend/fronted_master.blade.php
@@ -1,32 +1,31 @@
 <!doctype html>
 <html class="no-js" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-  <head>
-      <title>@yield('title')</title>
+
+<head>
+    <title>@yield('title')</title>
     @php
-        $admin_settings = App\Models\Admin::find(1);
+    $admin_settings = App\Models\Admin::find(1);
     @endphp
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="author" content="@if(session()->get('language') == 'bangla'){{ $admin_settings->meta_author_ban }}@else{{ $admin_settings->meta_author_en }}@endif">
+    <meta name="author"
+        content="@if(session()->get('language') == 'bangla'){{ $admin_settings->meta_author_ban }}@else{{ $admin_settings->meta_author_en }}@endif">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="google-site-verification" content="6AORxK8A6j50zcRhCkYcpzBGptly4PHr0o-jj7pKXJY" />
     <!-- Facebook Page -->
     <meta property="fb:pages" content="103901634319770" />
     @yield('custrom_metadata')
     <!-- Favicon -->
-    <link rel="shortcut icon" type="image/x-icon" href="{{ $admin_settings->fav_icon != NULL ? asset($admin_settings->fav_icon) : 'upload/defualt/fav_icon.png' }}">
+    <link rel="shortcut icon" type="image/x-icon"
+        href="{{ $admin_settings->fav_icon != NULL ? asset($admin_settings->fav_icon) : 'upload/defualt/fav_icon.png' }}">
     <!-- Bootstrap CSS -->
-    <link
-    rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
     <!-- Icon Font CSS -->
-    <link
-    rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
     <!-- Plugins CSS -->
-    <link
-    rel="stylesheet" href="{{ asset('frontend') }}/css/plugins.css">
+    <link rel="stylesheet" href="{{ asset('frontend') }}/css/plugins.css">
     <!-- Style CSS -->
-    <link
-    rel="stylesheet" href="{{ asset('frontend') }}/style.css">
+    <link rel="stylesheet" href="{{ asset('frontend') }}/style.css">
     <!-- Modernizer JS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
     <!-- Custrom style -->
@@ -36,46 +35,47 @@
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-204164438-1"></script>
     <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
+        window.dataLayer = window.dataLayer || [];
 
-    gtag('config', 'UA-204164438-1');
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-204164438-1');
+
     </script>
-
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-WQWQ5CT');</script>
-    <!-- End Google Tag Manager -->
 
     <!-- Google AdSense -->
     <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5937244308155885"
-     crossorigin="anonymous"></script>
+        crossorigin="anonymous"></script>
     <!-- End Google AdSense -->
 
 
-  </head>
-  <body>
-    <!-- Main Wrapper -->
-    <div
-      id="main-wrapper">
-      <!-- Header Top Start -->
-      @include('frontend.common.topbar')
-      <!-- Header Top End -->
-      <!-- Menu Section Start -->
-      @include('frontend.common.navbar')
-      <!-- Menu Section End -->
-      @yield('content')
+</head>
 
-      <!-- Brand Section Start -->
-      {{-- @include('frontend.common.brand') --}}
-      <!-- Brand Section End -->
-      <!-- Footer Section Start -->
-    @include('frontend.common.footer')
-      <!-- Footer Section End -->
+<body>
+    <div id="fb-root"></div>
+    <script async defer crossorigin="anonymous"
+        src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v11.0&appId=422059265828054&autoLogAppEvents=1"
+        nonce="5T3syam6"></script>
+
+    <!-- Main Wrapper -->
+    <div id="main-wrapper">
+        <!-- Header Top Start -->
+        @include('frontend.common.topbar')
+        <!-- Header Top End -->
+        <!-- Menu Section Start -->
+        @include('frontend.common.navbar')
+        <!-- Menu Section End -->
+        @yield('content')
+
+        <!-- Brand Section Start -->
+        {{-- @include('frontend.common.brand') --}}
+        <!-- Brand Section End -->
+        <!-- Footer Section Start -->
+        @include('frontend.common.footer')
+        <!-- Footer Section End -->
     </div>
     <!-- jQuery JS -->
     <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
@@ -91,5 +91,6 @@
     <script src="{{ asset('frontend') }}/js/main.js"></script>
     @yield('custrom_script')
 
-  </body>
+</body>
+
 </html>
--- resources/views/frontend/side_widget/social_join.blade.php
+++ resources/views/frontend/side_widget/social_join.blade.php
@@ -1,53 +1,22 @@
 <div class="single-sidebar col-lg-12 col-md-6 col-12">
-
     <!-- Sidebar Block Wrapper -->
     <div class="sidebar-block-wrapper">
-
         <!-- Sidebar Block Head Start -->
         <div class="head feature-head">
-
             <!-- Title -->
             <h4 class="title">@if (session()->get('language') == 'bangla')আমাদের সাথে যোগ দান @else
                 JOIN US ON @endif </h4>
-
         </div><!-- Sidebar Block Head End -->
         @php
-            $admin = App\Models\Admin::where('id', 1)->first();
+        $admin = App\Models\Admin::where('id', 1)->first();
         @endphp
         <!-- Sidebar Block Body Start -->
         <div class="body">
-
-            <div class="sidebar-social-follow">
-                <div>
-                    <a target="_blank" href="{{ $admin->facebook_url }}" class="facebook">
-                        <i class="fa fa-facebook"></i>
-                        <h3>40,583</h3>
-                        <span>Fans</span>
-                    </a>
-                </div>
-                <div>
-                    <a target="_blank" href="{{ $admin->youtube_url }}" class="google-plus">
-                        <i class="fa fa-google-plus"></i>
-                        <h3>36,857</h3>
-                        <span>Followers</span>
-                    </a>
-                </div>
-                <div>
-                    <a target="_blank" href="{{ $admin->twitter_url }}" class="twitter">
-                        <i class="fa fa-twitter"></i>
-                        <h3>50,883</h3>
-                        <span>Followers</span>
-                    </a>
-                </div>
-                <div>
-                    <a target="_blank" href="{{ $admin->pinterest_url }}" class="dribbble">
-                        <i class="fa fa-dribbble"></i>
-                        <h3>4,743</h3>
-                        <span>Followers</span>
-                    </a>
-                </div>
-            </div>
-
+            <iframe
+                src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ftrickitworld%2F&tabs=timeline&width=300&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=422059265828054"
+                width="400" height="400" style="border:none;overflow:hidden" scrolling="no" frameborder="0"
+                allowfullscreen="true"
+                allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
         </div><!-- Sidebar Block Body End -->
 
     </div>
